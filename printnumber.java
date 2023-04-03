d5cb75737459f71963b5225c267f7d770aa8e716
Arijit Samanta <104666115+ARIJITSAMANTA@users.noreply.github.com>
2023-04-03 16:19:02 +0530

    Add files via upload

---
+++ printnumber.java
@@ -0,0 +1,12 @@
+public class printnumber {
+    public static void main(String args[]){
+        int count=1;
+        while(count<=100){
+            System.out.print(count+" ");
+            count++;
+        }
+
+        System.out.println();
+    }
+    
+}
